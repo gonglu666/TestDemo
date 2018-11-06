e85e984eb810dd6edd2a411e6e8e4e875d5d1a64
Lu, Gong <lu.gong@arcserve.com>
2018-11-06 18:26:31 +0800

    ddd

--- TestDemo/src/com/kaka/arcserver/TestArcserver.java
+++ TestDemo/src/com/kaka/arcserver/TestArcserver.java
@@ -3,7 +3,7 @@ package com.kaka.arcserver;
 public class TestArcserver {
 
 	public static void main(String[] args) {
-		System.out.println("this is arcserver");
+		System.out.println("this is arcservers");
 	}
 
 }
